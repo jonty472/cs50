b2699e060b06d97f72c96b02d09f18f8d718b50d
jonty472 <azttorrs@gmail.com>
2023-05-16 23:08:35 +1200

    filter-less

--- pset4/filter-less/helpers.c
+++ pset4/filter-less/helpers.c
@@ -21,17 +21,67 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+            image[i][j].rgbtGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+            image[i][j].rgbtBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+        }
+    }
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width/2; j++)
+        {
+            // swap to hold original pixel value to reverse row array
+            int swapRed = image[i][j].rgbtRed;
+            int swapGreen = image[i][j].rgbtGreen;
+            int swapBlue = image[i][j].rgbtBlue;
+
+            image[i][j].rgbtRed = image[i][width - j].rgbtRed;
+            image[i][width - j].rgbtRed = swapRed;
+
+            image[i][j].rgbtGreen = image[i][width - j].rgbtGreen;
+            image[i][width - j].rgbtGreen = swapGreen;
+
+            image[i][j].rgbtBlue = image[i][width - j].rgbtBlue;
+            image[i][width - j].rgbtBlue = swapBlue;
+        }
+    }
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+    // testing to see if i've copied over the image correctly
+    printf("R:%d, G:%d, B:%d\n", copy[1]->rgbtRed, copy[1]->rgbtGreen, copy[1]->rgbtBlue);
+    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int redBlur = 0;
+            int greenBlur = 0;
+            int blueBlur = 0;
+            for (int k = -1; k <= 1; k++)
+            {
+                redBlur += copy[i-k][j-k].rgbtRed;
+                greenBlur += copy[i-k][j-k].rgbtGreen;
+                blueBlur += copy[i-k][j-k].rgbtBlue;
+            }
+            image[i][j].rgbtRed = redBlur / 3;
+            image[i][j].rgbtGreen = greenBlur / 3;
+            image[i][j].rgbtBlue = blueBlur / 3;
+        }
+    }
     return;
 }
